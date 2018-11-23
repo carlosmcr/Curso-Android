793d713c90290a81d8815b47e6d5ebdb3f9c118f
BitterBaitBite <guiyepk@hotmail.com>
2018-11-23 11:29:12 +0100

    Mensaje

--- app/src/main/java/es/indra/demoapp1/MainActivity.java
+++ app/src/main/java/es/indra/demoapp1/MainActivity.java
@@ -2,6 +2,8 @@ package es.indra.demoapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void recibirDatos(View view) {
+        EditText et = findViewById(R.id.idText);
+        String mensaje = et.getText().toString();
+        System.out.println(mensaje);
+    }
+
 }
