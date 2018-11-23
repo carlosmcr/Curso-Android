14ca85a159d2c2dd16c6c1673032628a7e4fc9ba
BitterBaitBite <guiyepk@hotmail.com>
2018-11-23 12:19:04 +0100

    Toast

--- app/src/main/java/es/indra/demoapp1/MainActivity.java
+++ app/src/main/java/es/indra/demoapp1/MainActivity.java
@@ -1,9 +1,11 @@
 package es.indra.demoapp1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     public void recibirDatos(View view) {
         EditText et = findViewById(R.id.idText);
         String mensaje = et.getText().toString();
-        System.out.println(mensaje);
+        Toast.makeText(getApplicationContext(), getString(R.string.toast) .concat(mensaje), Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(this, Main2Activity.class);
+        i.putExtra("param1", mensaje);
+        startActivity(i);
+
+
     }
 
 }
